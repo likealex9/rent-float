ba81c479802efcf3194060f28d660102e570203c
Marie <marietta.sv88@gmail.com>
2022-08-17 19:14:41 +0300

    aaaa

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+// import axios from 'axios';
 
 export default function MainPage() {
+  // useEffect(() => {
+  //   axios('http://localhost:3000/');
+  // }, [])
+
+  useEffect(() => {
+   fetch('http://localhost:3000/qwerty')
+  }, [])
+
   return (
-    <div>MainPage</div>
-  )
+    <div>MainPage </div>
+  );
 }
--- src/server.js
+++ src/server.js
@@ -2,9 +2,12 @@ import express from 'express';
 import morgan from 'morgan';
 import { renderToString } from 'react-dom/server';
 import React from 'react';
+import cors from 'cors';
 import Layout from './components/Layout';
 import loginrouter from './routes/loginroute';
 import regrouter from './routes/regrouter';
+import { flat } from './db/models';
+
 // npm i express-session session-file-store
 const app = express();
 const PORT = 3000;
@@ -12,6 +15,7 @@ const PORT = 3000;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 app.use('/login', loginrouter);
 app.use('/register', regrouter);
@@ -34,6 +38,12 @@ app.get('/houses/:id', async (req, res) => {
   } catch (err) {
     console.error(err);
   }
+});
+
+app.get('/qwerty', async (req, res) => {
+  const flats = await flat.findAll();
+  console.log(flats);
+  console.log('aaaaaaa');
 });
 
 app.listen(PORT, () => console.log(`Server is started on port ${PORT}`));
